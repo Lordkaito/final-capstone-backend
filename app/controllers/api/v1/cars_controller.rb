5304c7e249f6b4524e94c6157648c6716570cbb2
see-why <iyadicy@gmail.com>
2022-02-17 15:36:59 +0100

    Render all cars from index endpoint

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::CarsController < ApplicationController
   def index
+    @cars = Car.all
     respond_to do |format|
-      format.json { render json: { 'Hello world' } }
+      format.json { render json: @cars }
     end
   end
 end
