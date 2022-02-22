84c7eb9ad74f03e00772e487737d16a65242baa2
see-why <iyadicy@gmail.com>
2022-02-22 01:55:54 +0100

    Add city and reservation date to reservations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_17_141355) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_22_002052) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_17_141355) do
     t.bigint "car_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "city"
+    t.datetime "reservation_date"
     t.index ["car_id"], name: "index_reservations_on_car_id"
   end
 
