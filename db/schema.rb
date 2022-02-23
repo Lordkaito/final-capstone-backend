0b93b489a62e8e851e8ca846f20b19c09ea6790f
see-why <iyadicy@gmail.com>
2022-02-23 14:56:33 +0100

    Add description to car model, schema and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_22_002052) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_23_133538) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_22_002052) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
   create_table "reservations", force: :cascade do |t|
